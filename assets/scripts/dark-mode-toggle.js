d1e4656efca49d736c2f355c19938e82c64349eb
Brad Harris <bmharris@gmail.com>
2022-05-21 19:11:04 -0600

    dark-mode-toggle web component

--- assets/scripts/dark-mode-toggle.js
+++ assets/scripts/dark-mode-toggle.js
@@ -1,46 +1,109 @@
-let darkModeEnabled = false;
-const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+const template = document.createElement('template');
+template.innerHTML = `
+<style>
+.container {}
 
-function toggleDarkMode(enabled) {
-  document.documentElement.classList.toggle("dark-mode", enabled);
-
-  darkModeEnabled = enabled;
+.container .checkbox {
+  opacity: 0;
+  position: absolute;
 }
 
-function setDarkModeEnabled(enabled) {
-  localStorage.setItem("dark-mode", JSON.stringify(enabled));
+.container .label {
+  width: 25px;
+  height: 10px;
+  background-color: var(--color-text);
+  display: flex;
+  border-radius: 10px;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5px;
+  position: relative;
 }
 
-function clearDarkModeEnabled() {
-  localStorage.removeItem("dark-mode");
+.container .ball {
+  width: 16px;
+  height: 16px;
+  background-color: var(--color-bg);
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  border-radius: 50%;
+  transition: transform 0.2s linear;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const $toggle = document.getElementById('dm-toggle');
+.container .checkbox:checked+.label .ball {
+  transform: translateX(15px);
+}
 
-  $toggle.addEventListener("change", () => {
-    console.log('clicked')
-    darkModeEnabled = !darkModeEnabled;
+.container .dark-label,
+.container .light-label {
+  font-size: 12px;
+  font-weight: 400;
+  color: var(--color-bg);
+  font-family: var(--font-fam-headings);
+}
+</style>
+<div class="container">
+  <input type="checkbox" class="checkbox" id="dm-toggle">
+  <label for="dm-toggle" class="label">
+    <div class='ball'></div>
+    <span class="dark-label">D</span>
+    <span class="light-label">L</span>
+  </label>
+</div>
+`;
+class DarkModeToggle extends HTMLElement {
 
-    toggleDarkMode(darkModeEnabled);
-    setDarkModeEnabled(darkModeEnabled);
-  });
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
 
-  prefersDark.addListener((evt) => toggleDarkMode(evt.matches));
+    this.$container = this.shadowRoot.querySelector('.container');
+    this.$toggle = this.shadowRoot.querySelector('input[type="checkbox"]')
 
-  // initialize setting
-  const storageSetting = localStorage.getItem("dark-mode");
-  console.log('storageSetting', storageSetting);
-  if(storageSetting !== null) {
-    toggleDarkMode(storageSetting === "true");
-    $toggle.checked =storageSetting === "true";
-  }else {
-    toggleDarkMode(prefersDark.matches);
-    $toggle.checked = prefersDark.matches;
+    this.prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+    this.darkModeEnabled = this.getInitialValue();
+
+    this.$toggle.checked = this.darkModeEnabled;
+    this.setDarkModeEnabled(this.darkModeEnabled);
+
+    this.$toggle.addEventListener('change', () => {
+      this.setDarkModeEnabled(!this.darkModeEnabled);
+    });
   }
 
-  setTimeout(() => {
-    document.querySelector('.dark-mode-toggle-container').style.opacity = 1;
-  }, 300);
-});
+  getInitialValue() {
+    const storageSetting = localStorage.getItem(this.key);
 
+    let initialValue = false
+    // use whatever is in storage if present
+    if(storageSetting !== null) {
+      initialValue = storageSetting === "true";
+    }else {
+      // otherwise fall back to os preferences
+      initialValue = this.prefersDark.matches;
+    }
+
+    return initialValue;
+  }
+
+  get key() {
+    return this.getAttribute('key');
+  }
+
+  get darkmodeClass() {
+    return this.getAttribute('darkmode-class');
+  }
+
+  setDarkModeEnabled(value) {
+    this.darkModeEnabled = value;
+
+    document.documentElement.classList.toggle(this.darkmodeClass, value);
+    localStorage.setItem(this.key, JSON.stringify(value));
+  }
+
+}
+
+window.customElements.define('dark-mode-toggle', DarkModeToggle);
